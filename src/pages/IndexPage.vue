0b94aea981838a2db9cdd9e76cfe2e5eedc03d82
Boris Michael Randebrock <boris@randebrock.com>
2022-10-11 18:41:59 +0200

    initial setup-correction

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>hello world</h1>
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+
+<style scoped>
+q-page {
+  display: flex;
+  flex-direction: column;
+}
+</style>
