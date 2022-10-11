7e9b4fd1c327cb542323e338bbb94a2873659690
Boris Michael Randebrock <boris@randebrock.com>
2022-10-11 22:12:00 +0200

    added store/tasks and users, superbase.js and dotenv

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <router-view />
 </template>
 
@@ -8,4 +8,37 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'App'
 })
+</script> -->
+<template>
+  <section>
+    <router-view class="app-main" />
+    <!-- your routes will load inside of these tags -->
+  </section>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+// import { useUserStore } from "./store/user.js";
+import { useUserStore } from "./stores/user.js";
+
+const router = useRouter();
+const userStore = useUserStore();
+const { user } = storeToRefs(userStore);
+
+onMounted(async () => {
+  try {
+    await userStore.fetchUser(); // here we call fetch user
+    if (!user.value) {
+      // redirect them to logout if the user is not there
+      router.push({ path: "/auth" });
+    } else {
+      // continue to dashboard
+      router.push({ path: "/" });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
 </script>
---
+++ src/stores/task.js
@@ -0,0 +1,19 @@
+// /store/task.js
+
+import { defineStore } from "pinia";
+import { supabase } from "../supabase.js";
+
+export const useTaskStore = defineStore("tasks", {
+  state: () => ({
+    tasks: null,
+  }),
+  actions: {
+    async fetchTasks() {
+      const { data: tasks } = await supabase
+        .from("tasks")
+        .select("*")
+        .order("id", { ascending: false });
+      this.tasks = tasks;
+    },
+  },
+});
