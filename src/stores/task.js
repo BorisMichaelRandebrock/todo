21a6c942f7f5d6982e9c39095304920ab142c92b
Boris Michael Randebrock <boris@randebrock.com>
2022-10-27 18:56:19 +0200

    create and read done with supabase

--- src/stores/task.js
+++ src/stores/task.js
@@ -2,18 +2,42 @@
 
 import { defineStore } from "pinia";
 import { supabase } from "../supabase.js";
+import { useQuasar } from "quasar";
+
+const $q = useQuasar();
 
 export const useTaskStore = defineStore("tasks", {
   state: () => ({
-    tasks: null,
+    tasks: [],
   }),
   actions: {
-    async fetchTasks() {
+    async fetchTasks(id) {
       const { data: tasks } = await supabase
         .from("tasks")
         .select("*")
+        .eq("user_id", id)
         .order("id", { ascending: false });
       this.tasks = tasks;
+    },
+
+    async createTask(newTask) {
+      try {
+        const { data, error } = await supabase.from("tasks").insert({
+          user_id: newTask.user_id,
+          title: newTask.title,
+          is_complete: newTask.is_complete,
+          inserted_at: newTask.inserted_at,
+        });
+        if (error) throw error;
+        // this.tasks.push({ user_id: newTask.user_id, title: newTask.value });
+      } catch (error) {
+        $q.notify({
+          message: error.message,
+          color: "negative",
+          position: "top",
+          icon: "report_problem",
+        });
+      }
     },
   },
 });
